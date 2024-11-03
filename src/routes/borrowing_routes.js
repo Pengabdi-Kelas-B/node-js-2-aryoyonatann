d269ba0d175fb02a7fc8e7b0beeddbdeb430082e
aryoyonatann <3337220086@untirta.ac.id>
2024-11-03 15:20:32 +0700

    menambahkan dokumentasi postman

--- src/routes/borrowing_routes.js
+++ src/routes/borrowing_routes.js
@@ -3,12 +3,6 @@ const BorrowingController = require("../controllers/borrowing_controller");
 
 const borrowingRouter = express.Router();
 
-// borrowingRouter.get("/borrowings", BorrowingController.getAll);
-// borrowingRouter.get("/borrowing/:id", BorrowingController.getById);
-// borrowingRouter.post("/borrowing", BorrowingController.create);
-// borrowingRouter.put("/borrowing/:id", BorrowingController.update);
-// borrowingRouter.post("/borrow/book/return", BorrowingController.return);
-
 borrowingRouter.get("/borrow/book/list", BorrowingController.getAll);
 borrowingRouter.post("/borrow/book", BorrowingController.create);
 borrowingRouter.post("/borrow/book/return", BorrowingController.return);
